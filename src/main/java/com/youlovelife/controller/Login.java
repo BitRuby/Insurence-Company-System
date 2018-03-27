46bec09a2f539cf0ec43a6e58f4f79f22fc66f0e
Michal Zwierzynski <firewallosiem@gmail.com>
2018-03-27 16:16:18 +0200

    Added login controller

---
+++ src/main/java/com/youlovelife/controller/Login.java
@@ -0,0 +1,12 @@
+package com.youlovelife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Login {
+    @RequestMapping("/login")
+    public String demo(){
+        return "login.html";
+    }
+}
