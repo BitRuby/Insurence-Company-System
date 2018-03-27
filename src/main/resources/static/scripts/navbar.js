4c342ed3a4e41581cc5650d3c43da20571a5e3c6
Michal Zwierzynski <firewallosiem@gmail.com>
2018-03-27 20:04:01 +0200

    Added responsive menu in main page

--- src/main/resources/static/scripts/navbar.js
+++ src/main/resources/static/scripts/navbar.js
@@ -2,22 +2,37 @@ $( document ).ready(
     function() {
 
     var searchicon = $("#search-icon");
+    var menuicon = $("#menu-icon");
+    var mainmenu = $('#main-menu');
     var backicon = $("#back-icon");
     var navAll = $(".nav-all-collapse");
     var navSearch = $(".nav-search-collapse");
+
+    var navbarHeader = $(".navbar-header");
 
     searchicon.click(
         function(){
             navAll.css("display", "none");
             navSearch.css("display", "block");
+            navbarHeader.css("float", "left");
         }
     );
     backicon.click(
         function(){
             navAll.css("display", "block");
             navSearch.css("display", "none");
+            navbarHeader.css("float", "none");
         }
     );
-
+        menuicon.click(
+            function(){
+                if(!mainmenu.is(':visible')) {
+                    mainmenu.css("display", "block");
+                }
+                else {
+                    mainmenu.css("display", "none");
+                }
+            }
+        );
     }
 );
