d2a118c09d13afc4d9ad416329f501b2a748b1d6
Michal Zwierzynski <firewallosiem@gmail.com>
2018-05-17 15:37:54 +0200

    AngularJS Added token authorization with JWT

--- angular-app-up/components/login/login.js
+++ angular-app-up/components/login/login.js
@@ -1,40 +1,85 @@
 
-    angular.module('App')
-    .factory("Authorize", Authorize);
+angular.module('App')
+//.factory("Authorize", Authorize)
+.service("AuthService", AuthService);
 
-    function Authorize(){
-        var user;
-        var id;
-        var cond = false;
+function AuthService(){
+    return {
+        user: null
+    }
+}
+/*
+function Authorize(){
+    var user;
+    var id;
+    var cond = false;
 
-        var authorize = {
-            setUser: setUser,
-            currentUser: currentUser,
-            setLoggedOutInfo: setLoggedOutInfo,
-            getLoggedOutInfo: getLoggedOutInfo
-        }
-        return authorize;
+    var authorize = {
+        setUser: setUser,
+        currentUser: currentUser,
+        setLoggedOutInfo: setLoggedOutInfo,
+        getLoggedOutInfo: getLoggedOutInfo
+    }
+    return authorize;
 
-        function setUser(id){
-            this.id = id;
-        }
-        function currentUser(){
-            return this.id;
-        }
-        function setLoggedOutInfo(cond){
-            this.cond = cond;
-        }
-        function getLoggedOutInfo(){
-            return this.cond;
+    function setUser(id){
+        this.id = id;
+    }
+    function currentUser(){
+        return this.id;
+    }
+    function setLoggedOutInfo(cond){
+        this.cond = cond;
+    }
+    function getLoggedOutInfo(){
+        return this.cond;
+    }
+}
+*/
+(function(){
+    'use strict';
+    angular
+    .module('App', [])
+    .controller('loginController', loginController);
+
+    function loginController($http, $q, $state, $scope, $rootScope, AuthService){
+        var self = this;
+        self.submit = function(){
+            var params = {
+                username: self.username,
+                password: self.password
+            }
+            var REST_SERVICE_URI = 'http://127.0.0.1:8081/authenticate';
+            var deferredObject = $q.defer();
+            $http.post(REST_SERVICE_URI, params).then(
+                function (response){
+                    self.password = null;
+                    deferredObject.resolve(response.data);
+                    if (response.data.token){
+                        self.message = '';
+                        $http.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
+                        AuthService.user = response.data.user;
+                        $state.go('dashboard');
+                    }
+                    else {
+                        self.message = 'Invalid credentials provided';
+                    }
+                },
+                function (errResponse){
+                    self.message = 'Invalid credentials provided';
+                    console.error('Error while login');
+                    defferedObject.reject(errResponse);
+                }
+            );
+            return deferredObject.promise;
         }
     }
 
-(function(){
-    'use strict';
-
+/*
     angular.module('App')
+    .controller('temp', temp)
     .controller('loginController', loginController);
-    function loginController($http, $location, $rootScope, Authorize, $scope){
+    function loginController($http, $location, $rootScope, Authorize, $scope, $q){
         var self = this;        
         self.submit = function(){
             $http.get('http://127.0.0.1:5500/angular-app/json/users.json')
@@ -64,5 +109,20 @@
             self.showTouchID=true;
         }
     }
-    
+    function temp($http, $q){
+        var user = {username: "John", password: "Doe"};
+        var REST_SERVICE_URI = "";
+        var deferredObject = $q.defer();
+        $http.get(REST_SERVICE_URI, user).then(
+            function(response){
+                deferredObject.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while send user');
+                defferedObject.reject(errResponse);
+            }
+        );
+        return deferredObject.promise;
+    }
+*/
 })();
