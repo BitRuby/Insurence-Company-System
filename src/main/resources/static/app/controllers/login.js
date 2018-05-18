d97243ec90f5902571e60b486c107f7477c38bd4
firew <firewallosiem@gmail.com>
2018-05-18 16:46:50 +0200

    Added classes

---
+++ src/main/resources/static/app/controllers/login.js
@@ -0,0 +1,42 @@
+angular.module('JWTDemoApp')
+// Creating the Angular Controller
+    .controller('LoginController', function ($q,$http, $scope, $state, AuthService, $rootScope) {
+        // method for login
+        $scope.login = function () {
+            var deferredObject = $q.defer();
+            // requesting the token by usename and passoword
+            $http({
+                url: 'http://localhost:8080/authenticate',
+                method: "POST",
+                params: {
+                    username: $scope.username,
+                    password: $scope.password
+                }
+            }).success(function (res) {
+                deferredObject.resolve(res);
+                $scope.password = null;
+                // checking if the token is available in the response
+                if (res.token) {
+                    $scope.message = '';
+                    // setting the Authorization Bearer token with JWT token
+                    $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.token;
+
+                    // setting the user in AuthService
+                    AuthService.user = res.user;
+                    $rootScope.$broadcast('LoginSuccessful');
+                    // going to the home page
+                    $state.go('home');
+                } else {
+                   
+                    // if the token is not present in the response then the
+                    // authentication was not successful. Setting the error message.
+                    $scope.message = 'Authetication Failed !';
+                }
+            }).error(function (error) {
+                defferedObject.reject(error);
+                // if authentication was not successful. Setting the error message.
+                $scope.message = 'Authetication Failed !';
+            });
+            return deferredObject.promise;
+        };
+    });
