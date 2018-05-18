4ea90ddfbaec1ad61905ab054c27bca72237c38d
firew <firewallosiem@gmail.com>
2018-05-18 16:57:14 +0200

    AngularJS Added dashbord username show

--- angular-app-up/components/dashboard/dashboard.js
+++ angular-app-up/components/dashboard/dashboard.js
@@ -3,7 +3,9 @@
     angular.module('App')
     .controller('dashboardController', dashboardController);
 
-    function dashboardController($location, $rootScope){
+    function dashboardController($location, $rootScope, AuthService){
+        var self = this;
+        self.username = AuthService.user.name;
         /*
         var self = this;
         self.submit = function(){
