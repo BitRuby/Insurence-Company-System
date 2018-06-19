aa6d6165148f2dc90ed4681ecfa2230dde19ba0d
firew <firewallosiem@gmail.com>
2018-06-19 11:06:32 +0200

    Updated notifications

--- angular-app-up/components/admin/notifications/admin-notifications.js
+++ angular-app-up/components/admin/notifications/admin-notifications.js
@@ -8,10 +8,12 @@
         self.userVal = function(){
             if(self.userInput.length>1){
                 RestServices.findUser(self.userInput).then(function(){
+                    console.log(RestServices.data());
                     self.userList = RestServices.data();
                 });
             }
         }
     }
+
 
 })();
