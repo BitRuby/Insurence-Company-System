ee9ee6d98809d1ecbc96c308537908054b3c2d58
firew <firewallosiem@gmail.com>
2018-06-22 12:25:04 +0200

    Added company details

--- angular-app-up/components/company/company.js
+++ angular-app-up/components/company/company.js
@@ -4,6 +4,7 @@
     .controller('companyController', companyController);
 
     function companyController($scope, AuthService){
+        var self = this;
         $scope.tab = 1;
         $scope.setTab = function(newTab){
             $scope.tab = newTab;
@@ -11,7 +12,18 @@
         $scope.isSet = function(tabNum){
           return $scope.tab === tabNum;
         }
-        console.log(AuthService.user);
+        if (AuthService.user.currentCompany){
+            self.companyTabs = true;
+            self.companyName = AuthService.user.currentCompany.companyName;
+            self.companyAddressCity = AuthService.user.currentCompany.address.city;
+            self.companyAddressBuilding =  AuthService.user.currentCompany.address.building;
+            self.companyAddressStreet =  AuthService.user.currentCompany.address.street;
+            self.companyAddressPost = AuthService.user.currentCompany.address.postcode;
+            self.companyAddressCountry = AuthService.user.currentCompany.address.country;
+            self.companyTID = AuthService.user.currentCompany.nip;
+            self.companyBRN = AuthService.user.currentCompany.regon;
+            self.companyID = AuthService.user.currentCompany.companyID;
+        }
     }
 
 })();
