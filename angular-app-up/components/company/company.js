f60bd5401a7e196f45773fe7deeaf3caf70b9d26
firew <firewallosiem@gmail.com>
2018-06-24 14:38:41 +0200

    Adde company view employers

--- angular-app-up/components/company/company.js
+++ angular-app-up/components/company/company.js
@@ -13,24 +13,25 @@
           return $scope.tab === tabNum;
         }
         if (AuthService.user.ownCompany){
-            console.log(AuthService.user);
             self.companyTabs = true;
-            self.companyName = AuthService.user.currentCompany.companyName;
-            self.companyAddressCity = AuthService.user.currentCompany.address.city;
-            self.companyAddressBuilding =  AuthService.user.currentCompany.address.building;
-            self.companyAddressStreet =  AuthService.user.currentCompany.address.street;
-            self.companyAddressPost = AuthService.user.currentCompany.address.postcode;
-            self.companyAddressCountry = AuthService.user.currentCompany.address.country;
-            self.companyTID = AuthService.user.currentCompany.nip;
-            self.companyBRN = AuthService.user.currentCompany.regon;
-            self.companyID = AuthService.user.currentCompany.companyID;
+            self.companyName = AuthService.user.ownCompany.companyName;
+            self.companyAddressCity = AuthService.user.ownCompany.address.city;
+            self.companyAddressBuilding =  AuthService.user.ownCompany.address.building;
+            self.companyAddressStreet =  AuthService.user.ownCompany.address.street;
+            self.companyAddressPost = AuthService.user.ownCompany.address.postcode;
+            self.companyAddressCountry = AuthService.user.ownCompany.address.country;
+            self.companyTID = AuthService.user.ownCompany.nip;
+            self.companyBRN = AuthService.user.ownCompany.regon;
+            self.companyID = AuthService.user.ownCompany.companyID;
         }
         else{
             self.noCompanyInfo = true;
         }
         RestServices.getAllCompanies().then(function(){
-            console.log(RestServices.data());
-        
+            for (var i=0; i<RestServices.data().length; i++){
+                if (RestServices.data()[i].companyID == AuthService.user.ownCompany.companyID)
+                    self.employees = RestServices.data()[i].employees; 
+            }
         });
     }
 
