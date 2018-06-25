7744977c3bd59cc7c6fd1fa11bfca4ac35f7ae2f
firew <firewallosiem@gmail.com>
2018-06-25 15:18:34 +0200

    Added applications manager in admin panel

--- angular-app-up/components/admin/applications/admin-applications.js
+++ angular-app-up/components/admin/applications/admin-applications.js
@@ -3,8 +3,12 @@
     angular.module('App')
     .controller('adminApplicationsController', adminApplicationsController);
 
-    function adminApplicationsController(){
-
+    function adminApplicationsController(AuthService, RestServices){
+        var self = this;
+        RestServices.getAllApplications().then(function(){
+            console.log(RestServices.data());
+            self.applicants = RestServices.data();
+        });
     }
 
 })();
